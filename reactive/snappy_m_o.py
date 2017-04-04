3b16af56503b91f9843033127d6c1f71708202b6
Leo Arias <leo.arias@canonical.com>
2017-04-04 20:36:06 +0000

    Add the missing imports

--- reactive/snappy_m_o.py
+++ reactive/snappy_m_o.py
@@ -15,6 +15,11 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import subprocess
+
+from charms.reactive import when_not, set_state
+
+
 @when_not('snappy-m-o.installed')
 def install_snappy_m_o():
     subprocess.check_call(['snap', 'install', 'snappy-m-o'])
